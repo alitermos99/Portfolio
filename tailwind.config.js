05e4e76753328230a3d87eab5ddb22da6da5f367
Ali Termos <ali1999.termos@hotmail.com>
2026-02-14 17:23:51 +0200

    build(v1): added required colors for the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./force-app/main/default/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./force-app/main/default/**/*.{html,js}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				mont: ['var(--font-mont)', fontFamily.sans],
+			},
+			colors: {
+				dark: "#1b1b1b",
+				light: "#f5f5f5",
+				primary: "#B63E96", // 240,86,199
+				primaryDark: "#58E6D9", // 80,230,217
+			}
+		},
+	},
+	plugins: [],
 }
