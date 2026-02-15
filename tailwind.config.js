8c0020468d15cef6d12b8dcf9088e72b24e1a19e
Ali Termos <ali1999.termos@hotmail.com>
2026-02-15 11:59:10 +0200

    build(v1): create and implement portfolio logo and import tailwind css to create effect similar to framer motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,28 @@
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./force-app/main/default/**/*.{html,js}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				mont: ['var(--font-mont)', fontFamily.sans],
-			},
-			colors: {
-				dark: "#1b1b1b",
-				light: "#f5f5f5",
-				primary: "#B63E96", // 240,86,199
-				primaryDark: "#58E6D9", // 80,230,217
-			}
-		},
-	},
-	plugins: [],
-}
+module.exports = {
+  content: ["./force-app/main/default/**/*.{html,js}"],
+  safelist: [
+    "bg-dark",
+    "text-light",
+    "bg-primary",
+    "bg-primaryDark",
+    "text-primary",
+    "text-primaryDark"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", fontFamily.sans]
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9" // 80,230,217
+      }
+    }
+  },
+  plugins: []
+};
