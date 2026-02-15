676755cebe43f8deb4d949dbf60976e7f682cc0c
Ali Termos <ali1999.termos@hotmail.com>
2026-02-15 13:38:14 +0200

    build(v1): added cursor-pointer class for slot element in link lwc, implemented portfolioLink lwc to be able to use the link lwc with custom logic, added spanclassname to put a full width line on the current nav element based on the url using currentpagerefenece, in addition to adding dummy data in the current portfolioNavbar lwc and reference the lwc in the portfolioAppContainer

--- force-app/main/default/lwc/portfolioLink/portfolioLink.js
+++ force-app/main/default/lwc/portfolioLink/portfolioLink.js
@@ -1,3 +1,25 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import { CurrentPageReference } from "lightning/navigation";
 
-export default class PortfolioLink extends LightningElement {}
+export default class PortfolioLink extends LightningElement {
+  @api href;
+  @api title;
+  @api className;
+  _currentPageName;
+
+  get linkClassName() {
+    return `${this.className} relative group`;
+  }
+
+  get spanClassName() {
+    const href = this.href === "/" ? "Home" : this.href;
+    return `${this._currentPageName === href ? "w-full" : "w-0"} h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300`;
+  }
+
+  @wire(CurrentPageReference)
+  getStateParameters(currentPageReference) {
+    if (currentPageReference) {
+      this._currentPageName = currentPageReference.attributes?.name || null;
+    }
+  }
+}
