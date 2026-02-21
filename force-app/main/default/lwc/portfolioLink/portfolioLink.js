2c9378823947d6cbf2497a15c19186c576e5c61b
Ali Termos <ali1999.termos@hotmail.com>
2026-02-21 14:52:58 +0200

    build(v1): fix indentation for lwcs portfolioLink, portfolioLogo and porftolioNavbar

--- force-app/main/default/lwc/portfolioLink/portfolioLink.js
+++ force-app/main/default/lwc/portfolioLink/portfolioLink.js
@@ -2,24 +2,24 @@ import { LightningElement, api, wire } from "lwc";
 import { CurrentPageReference } from "lightning/navigation";
 
 export default class PortfolioLink extends LightningElement {
-  @api href;
-  @api title;
-  @api className;
-  _currentPageName;
+	@api href;
+	@api title;
+	@api className;
+	_currentPageName;
 
-  get linkClassName() {
-    return `${this.className} relative group`;
-  }
+	get linkClassName() {
+		return `${this.className} relative group`;
+	}
 
-  get spanClassName() {
-    const href = this.href === "/" ? "Home" : this.href;
-    return `${this._currentPageName === href ? "w-full" : "w-0"} h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300`;
-  }
+	get spanClassName() {
+		const href = this.href === "/" ? "Home" : this.href;
+		return `${this._currentPageName === href ? "w-full" : "w-0"} h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300`;
+	}
 
-  @wire(CurrentPageReference)
-  getStateParameters(currentPageReference) {
-    if (currentPageReference) {
-      this._currentPageName = currentPageReference.attributes?.name || null;
-    }
-  }
+	@wire(CurrentPageReference)
+	getStateParameters(currentPageReference) {
+		if (currentPageReference) {
+			this._currentPageName = currentPageReference.attributes?.name || null;
+		}
+	}
 }
