226f986e40586edc7859f860f17b23289a990f17
Ali Termos <ali1999.termos@hotmail.com>
2026-02-21 14:59:39 +0200

    build(v1): added the homepage bode with profilepic portfolioAppContainer lwc

--- force-app/main/default/lwc/portfolioAppContainer/portfolioAppContainer.js
+++ force-app/main/default/lwc/portfolioAppContainer/portfolioAppContainer.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import Portfolio from '@salesforce/resourceUrl/Portfolio';
 
-export default class PortfolioAppContainer extends LightningElement {}
+export default class PortfolioAppContainer extends LightningElement {
+	isHtmlLoaded = false
+	profilePic = Portfolio + '/profile/developer-pic-1.png';
+
+	renderedCallback(){
+		const html = document.querySelector('html');
+
+		if(html && !this.isHtmlLoaded){
+			this.isHtmlLoaded = true;
+			html.classList.add('bg-light');
+		}
+	}
+}
