c47d141d4dc37907fef0831b241172ac6c291aa7
Ali Termos <ali1999.termos@hotmail.com>
2026-02-21 14:51:12 +0200

    build(v1): implement animatedText lwc to staggerchildren with delay of 80ms and fadein animation

--- force-app/main/default/lwc/animatedText/animatedText.js
+++ force-app/main/default/lwc/animatedText/animatedText.js
@@ -1,3 +1,20 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class AnimatedText extends LightningElement {}
+export default class AnimatedText extends LightningElement {
+	@api text;
+	@api className = "";
+
+	get h1ClassName() {
+		return `inline-block w-full text-dark font-bold capitalize text-8xl opacity-0 animate-fade-in delay-500 ${this.className}`;
+	}
+
+	get splitText() {
+		return this.text?.split(" ").map((word, index) => {
+			return {
+				key: index,
+				label: word,
+				style: `animation-delay: ${index * 80}ms;`
+			};
+		});
+	}
+}
