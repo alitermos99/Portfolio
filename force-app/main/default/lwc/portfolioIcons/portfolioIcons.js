8b22c643076b8cf3e75340d33cc3735bf43da6ae
Ali Termos <ali1999.termos@hotmail.com>
2026-02-21 14:54:02 +0200

    build(v1): added the requiredicons and temporary disable sun and moon svgs portfolioIcons lwc

--- force-app/main/default/lwc/portfolioIcons/portfolioIcons.js
+++ force-app/main/default/lwc/portfolioIcons/portfolioIcons.js
@@ -1,3 +1,31 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class PortfolioIcons extends LightningElement {}
+export default class PortfolioIcons extends LightningElement {
+	@api iconName;
+	@api className;
+	@api restProps;
+
+	get github() {
+		return this.iconName === "github";
+	}
+
+	get linkedIn() {
+		return this.iconName === "linkedIn";
+	}
+
+	get sunIcon() {
+		return this.iconName === "sun";
+	}
+
+	get moonIcon() {
+		return this.iconName === "moon";
+	}
+
+	get circularText() {
+		return this.iconName === "circularText";
+	}
+
+	get linkArrowIcon() {
+		return this.iconName === "linkArrow";
+	}
+}
