68bd108b41da8ba1545a767adb2c6255a97471be
Ali Termos <ali1999.termos@hotmail.com>
2026-02-22 12:43:58 +0200

    build(v1): create a seperate component for home page and put the html, js data from portfolioAppContainer lwc into portfolioHomePage, in addition the implementation of the portfolioHireMe lwc

--- force-app/main/default/lwc/portfolioHomePage/portfolioHomePage.js
+++ force-app/main/default/lwc/portfolioHomePage/portfolioHomePage.js
@@ -1,3 +1,8 @@
 import { LightningElement } from "lwc";
+import Portfolio from "@salesforce/resourceUrl/Portfolio";
 
-export default class PortfolioHomePage extends LightningElement {}
+export default class PortfolioHomePage extends LightningElement {
+	bulbIcon = Portfolio + "/icons/bulb.svg";
+	profilePic = Portfolio + "/profile/developer-pic-1.png";
+	resumeLink = Portfolio + "/resume/alitermosSalesforceDeveloperCV.pdf";
+}
