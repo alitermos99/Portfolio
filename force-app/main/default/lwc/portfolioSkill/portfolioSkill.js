0079c19c2d08a350de801a010af4975d03b9c15d
Ali Termos <ali1999.termos@hotmail.com>
2026-02-23 13:41:22 +0200

    build(v1): create portfolioSkills lwc to hold the skills data, in addition to the portfolioSkill lwc that animates the skills submitted and configure css tailwind

--- force-app/main/default/lwc/portfolioSkill/portfolioSkill.js
+++ force-app/main/default/lwc/portfolioSkill/portfolioSkill.js
@@ -1,3 +1,42 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class PortfolioSkill extends LightningElement {}
+export default class PortfolioSkill extends LightningElement {
+	@api x;
+    @api y;
+    @api name;
+
+    translateX = '0vw';
+    translateY = '0vw';
+    scale = 1;
+
+    observer;
+
+    get dynamicStyle() {
+        return `transform: translate(${this.translateX}, ${this.translateY}) scale(${this.scale});`;
+    }
+
+    renderedCallback() {
+        if (this.observer) return;
+
+        const el = this.template.querySelector('div');
+
+        this.observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                this.translateX = this.x;
+                this.translateY = this.y;
+
+                this.observer.disconnect();
+            }
+        }, { threshold: 0.3 });
+
+        this.observer.observe(el);
+    }
+
+    handleMouseOver() {
+        this.scale = 1.05;
+    }
+
+    handleMouseOut() {
+        this.scale = 1;
+    }
+}
--- force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
+++ force-app/main/default/lwc/portfolioSkills/portfolioSkills.js
@@ -1,3 +1,36 @@
 import { LightningElement } from "lwc";
 
-export default class PortfolioSkills extends LightningElement {}
+export default class PortfolioSkills extends LightningElement {
+	skills = [
+		{
+			name: "HTML",
+			x: "-25vw",
+			y: "2vw"
+		},
+		{
+			name: "CSS",
+			x: "-5vw",
+			y: "-10vw"
+		},
+		{
+			name: "Lightning Web Component",
+			x: "-30vw",
+			y: "9vw"
+		},
+		{
+			name: "Lightning Component",
+			x: "20vw",
+			y: "-10vw"
+		},
+		{
+			name: "Visualforce",
+			x: "-27vw",
+			y: "-15vw"
+		},
+		{
+			name: "Apex",
+			x: "10vw",
+			y: "-17vw"
+		}
+	]
+}
