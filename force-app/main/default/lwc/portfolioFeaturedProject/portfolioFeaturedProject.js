5c8499acbe6d19281645fcfb34646f5e36732aac
Ali Termos <ali1999.termos@hotmail.com>
2026-02-27 12:48:18 +0200

    build(v1): implemented PortfolioFeaturedProject lwc to contain the info about the featured project, in addition to the implementation of the PortfolioProjectsPage to include the projects array

--- force-app/main/default/lwc/portfolioFeaturedProject/portfolioFeaturedProject.js
+++ force-app/main/default/lwc/portfolioFeaturedProject/portfolioFeaturedProject.js
@@ -1,3 +1,23 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class PortfolioFeaturedProject extends LightningElement {}
+export default class PortfolioFeaturedProject extends LightningElement {
+	@api name;
+	@api time;
+	@api title;
+	@api summary;
+	@api img;
+	@api link;
+	@api github;
+	@api responsibilities;
+
+	get showProjectLink() {
+		return this.link || this.github;
+	}
+
+	get responsibilitiesArray() {
+		return this.responsibilities?.split('~').map((resp, index) => ({
+			key: index,
+			value: resp
+		})) || [];
+	}
+}
--- force-app/main/default/lwc/portfolioProjectsPage/portfolioProjectsPage.js
+++ force-app/main/default/lwc/portfolioProjectsPage/portfolioProjectsPage.js
@@ -1,3 +1,23 @@
 import { LightningElement } from "lwc";
+import Portfolio from "@salesforce/resourceUrl/Portfolio";
 
-export default class PortfolioProjectsPage extends LightningElement {}
+export default class PortfolioProjectsPage extends LightningElement {
+
+	projects = [
+		{
+			id: crypto.randomUUID(),
+			name: "Air Liquide",
+			time: "2025 - Current",
+			isFeaturedProject: true,
+			title: "Integration Developer",
+			summary: "Air Liquide is a world leader in gases, technologies and services for industry and healthcare.",
+			techStack: ["Salesforce", "Lightning Web Components", "Salesforce CLI"],
+			img: Portfolio + "/projects/Air-Liquide-Logo.png",
+			link: "",
+			responsibilities: `Integrated Salesforce with an external system to enable seamless data exchange.~
+					Built FHIR-compliant JSON dynamically using custom metadata to 
+					structure the data. Ensured efficient transmission and processing through a robust integration framework.`,
+		},
+
+	]
+}
