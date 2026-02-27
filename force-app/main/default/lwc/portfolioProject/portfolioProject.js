3e96aa52deab53e5b4afc79d6e93564570e43969
Ali Termos <ali1999.termos@hotmail.com>
2026-02-27 14:56:42 +0200

    build(v1): implement PortfolioProject lwc and remove PortfolioFeaturedProject lwc to remove redundant components and put my worked-on projects with logos in the static resources

--- force-app/main/default/lwc/portfolioProject/portfolioProject.js
+++ force-app/main/default/lwc/portfolioProject/portfolioProject.js
@@ -1,3 +1,64 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class PortfolioProject extends LightningElement {}
+export default class PortfolioProject extends LightningElement {
+	@api name;
+	@api time;
+	@api title;
+	@api summary;
+	@api img;
+	@api link;
+	@api github;
+	@api responsibilities;
+	@api isFeaturedProject = false;
+
+	get showProjectLink() {
+		return this.link || this.github;
+	}
+
+	get responsibilitiesArray() {
+		return this.responsibilities?.split('~').map((resp, index) => ({
+			key: index,
+			value: resp
+		})) || [];
+	}
+
+	get articleClass() {
+		const baseClass = 'w-full flex items-center border border-solid border-dark bg-light';
+
+		return this.isFeaturedProject ?
+			`${baseClass} justify-between rounded-3xl shadow-2xl p-12` : `${baseClass} flex-col justify-center rounded-2xl p-6 relative`;
+	}
+
+	get imageWrapperClass() {
+		const baseClass = 'overflow-hidden rounded-lg cursor-pointer';
+		return this.isFeaturedProject ? `${baseClass} w-1/2` : `${baseClass} w-full`;
+	}
+
+	get infoDivClass() {
+		const baseClass = 'flex flex-col items-start justify-between';
+		return this.isFeaturedProject ? `${baseClass} w-1/2 pl-6` : `${baseClass} w-full mt-4`;
+	}
+
+	get h2Class() {
+		const baseClass = 'my-2 w-full text-left font-bold';
+		return this.isFeaturedProject ? `${baseClass} text-4xl` : `${baseClass} text-3xl`;
+	}
+
+	get linkDivClass() {
+		const baseClass = 'mt-2 flex items-center';
+		return this.isFeaturedProject ? `${baseClass} text-4xl` : `${baseClass} w-full justify-between flex-row-reverse`;
+	}
+
+	get linkClass() {
+		const baseClass = 'block px-6 text-lg font-semibold';
+		return this.isFeaturedProject ? `${baseClass} ml-4 rounded-lg bg-dark p-2 text-light` : `${baseClass} underline`;
+	}
+
+	get githubLinkClass() {
+		return this.isFeaturedProject ? 'w-10' : 'w-8';
+	}
+
+	get linkSpanText() {
+		return this.isFeaturedProject ? 'Visit Project' : 'Visit';
+	}
+}
--- force-app/main/default/lwc/portfolioProjectsPage/portfolioProjectsPage.js
+++ force-app/main/default/lwc/portfolioProjectsPage/portfolioProjectsPage.js
@@ -7,17 +7,61 @@ export default class PortfolioProjectsPage extends LightningElement {
 		{
 			id: crypto.randomUUID(),
 			name: "Air Liquide",
-			time: "2025 - Current",
+			time: "January 2025 - Current",
 			isFeaturedProject: true,
+			divClass: 'col-span-12',
 			title: "Integration Developer",
 			summary: "Air Liquide is a world leader in gases, technologies and services for industry and healthcare.",
 			techStack: ["Salesforce", "Lightning Web Components", "Salesforce CLI"],
-			img: Portfolio + "/projects/Air-Liquide-Logo.png",
+			img: Portfolio + "/projects/Air-Liquide-logo.png",
 			link: "",
 			responsibilities: `Integrated Salesforce with an external system to enable seamless data exchange.~
 					Built FHIR-compliant JSON dynamically using custom metadata to 
 					structure the data. Ensured efficient transmission and processing through a robust integration framework.`,
 		},
-
+		{
+			id: crypto.randomUUID(),
+			name: "Madi International",
+			time: "September 2024 - January 2025",
+			isFeaturedProject: false,
+			divClass: 'col-span-6',
+			title: "Front-End Developer",
+			summary: "Madi International is a multi-brand, multi-trade channel distributor covering all beauty categories including hair, skin, and nail care.",
+			techStack: ["Salesforce", "Lightning Web Components", "Salesforce CLI"],
+			img: Portfolio + "/projects/Madi-international-logo.png",
+			link: "",
+			responsibilities: `Crafted an intuitive and user-friendly UI that made navigation easier and optimized overall usability, enhancing client satisfaction.~
+					Built a B2B experience site using Salesforce B2B Commerce Cloud, designed to meet various business needs effectively.`,
+		},
+		{
+			id: crypto.randomUUID(),
+			name: "Nakheel/DHRE",
+			time: "June 2023 - September 2024",
+			isFeaturedProject: false,
+			divClass: 'col-span-6',
+			title: "Full-Stack Developer",
+			summary: "Nakheel Properties is a real estate development company based in Dubai, United Arab Emirates. Later in 2024, Nakheel was merged under Dubai Holdings.",
+			techStack: ["Salesforce", "Lightning Web Components", "Salesforce CLI"],
+			img: Portfolio + "/projects/Nakheel-logo.png",
+			link: "",
+			responsibilities: `Integrated Salesforce with Docusign for digital signing through the Apex toolkit and Docusign App, enhancing client satisfaction.~
+					Implemented over six customized business templates using Nintex, streamlining workflows and enhancing business operations.`,
+		},
+		{
+			id: crypto.randomUUID(),
+			name: "DMCC",
+			time: "October 2022 - June 2023",
+			isFeaturedProject: false,
+			divClass: 'col-span-6',
+			title: "Full-Stack Developer",
+			summary: "The Dubai Multi Commodities Centre (DMCC) is a commodities exchange and free-trade zone in the United Arab Emirates.",
+			techStack: ["Salesforce", "Lightning Web Components", "Salesforce CLI"],
+			img: Portfolio + "/projects/DMCC-logo.png",
+			link: "",
+			responsibilities: ` Transitioned from AURA and Visualforce pages to LWC, resulting in faster load times and improved user experiences, leading to an increase in user 
+					engagement.~
+					Delivered a responsive website that enhanced user experience, resulting in increased engagement and overall satisfaction.~
+					Designed reusable Lightning Web Components (LWC) to simplify deployment and enhance the development workflow.`,
+		},
 	]
 }
