f6bde9032e8bca4329bfab2c28b061652287679b
Ali Termos <ali1999.termos@hotmail.com>
2026-02-28 16:06:13 +0200

    build(v1): create PortfolioThemeSwitcher lwc and configure tailwinf.config.js file to use darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,9 +27,11 @@ module.exports = {
 				"spin-slow": "spin 8s linear infinite"
 			},
 			backgroundImage: {
-				circularLight: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #F5F5F5 5px, #F5F5F5 100px)"
+				circularLight:
+					"repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #F5F5F5 5px, #F5F5F5 100px)"
 			}
 		}
 	},
-	plugins: []
+	plugins: [],
+	darkMode: "class"
 };
