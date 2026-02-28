e8436271b67f31491b4d21f9eabfc2bef47f260c
Ali Termos <ali1999.termos@hotmail.com>
2026-02-28 17:00:28 +0200

    build(v1): added required css classes to the output.css file, implement portfolioThemeSwitcher lwc and use it in the portfolioNavbar lwc to be able to switch between dark and light, and added the first dark:bg-dark in the portfolioLayout lwc

--- force-app/main/default/lwc/portfolioLayout/portfolioLayout.js
+++ force-app/main/default/lwc/portfolioLayout/portfolioLayout.js
@@ -4,6 +4,6 @@ export default class PortfolioLayout extends LightningElement {
 	@api className;
 
 	get divClassName() {
-		return `w-full h-full inline-block z-0 bg-light p-32 ${this.className}`;
+		return `w-full h-full inline-block z-0 bg-light dark:bg-dark p-32 ${this.className}`;
 	}
 }
--- force-app/main/default/lwc/portfolioThemeSwitcher/portfolioThemeSwitcher.js
+++ force-app/main/default/lwc/portfolioThemeSwitcher/portfolioThemeSwitcher.js
@@ -1,3 +1,62 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class PortfolioThemeSwitcher extends LightningElement {}
+const STORAGE_KEY = 'theme';
+const MEDIA_QUERY = '(prefers-color-scheme: dark)';
+
+export default class PortfolioThemeSwitcher extends LightningElement {
+	@api className;
+	mode = '';
+    mediaQuery;
+    handleChange;
+
+	get iconName() {
+		return this.mode === 'dark' ? 'moon' : 'sun';
+	}
+
+    connectedCallback() {
+        this.mediaQuery = window.matchMedia(MEDIA_QUERY);
+        const userPref = window.localStorage.getItem(STORAGE_KEY);
+
+        // Equivalent to first useEffect
+        this.handleChange = () => {
+            let check;
+
+            if (userPref) {
+                check = userPref === 'dark' ? 'dark' : 'light';
+            } 
+			else {
+                check = this.mediaQuery.matches ? 'dark' : 'light';
+            }
+
+            this.setMode(check);
+        };
+
+        this.handleChange();
+        this.mediaQuery.addEventListener('change', this.handleChange);
+    }
+
+    disconnectedCallback() {
+        if (this.mediaQuery && this.handleChange) {
+            this.mediaQuery.removeEventListener('change', this.handleChange);
+        }
+    }
+
+    setMode(mode) {
+        this.mode = mode;
+
+        const html = document.documentElement;
+
+        if (mode === 'dark') {
+            window.localStorage.setItem(STORAGE_KEY, 'dark');
+            html.classList.add('dark');
+        } 
+		else {
+            window.localStorage.setItem(STORAGE_KEY, 'light');
+            html.classList.remove('dark');
+        }
+    }
+
+    toggleTheme() {
+        this.setMode(this.mode === 'dark' ? 'light' : 'dark');
+    }
+}
